6f7546f90538b346ff9b39eee45e46e11e0f49b5
Lucas LEBUGLE <lulebugl@e3r11p7.42.fr>
2019-11-04 14:23:14 +0100

    Makefile update

--- Makefile
+++ Makefile
@@ -5,54 +5,50 @@
 #                                                     +:+ +:+         +:+      #
 #    By: lulebugl <lulebugl@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2017/12/18 20:56:37 by nmei              #+#    #+#              #
-#    Updated: 2019/11/04 13:12:00 by lulebugl         ###   ########.fr        #
+#    Created: 2019/10/15 17:50:44 by lulebugl          #+#    #+#              #
+#    Updated: 2019/11/04 14:22:18 by lulebugl         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-RM = /bin/rm -f
-
 NAME = libftprintf.a
-SRCS_DIR = ./
-FILES = ft_printf parse_prespecifiers parse_specifier int_conv\
-uint_handlers str_char_handlers special_handlers\
-printf_gen_utils printf_utf_utils\
-printf_int_utils
-CFILES = $(patsubst %, $(SRCS_DIR)%.c, $(FILES))
-OFILES = $(patsubst %, %.o, $(FILES))
-INCLUDES = ./includes
-CFLAGS = -Wall -Wextra -Werror -O2
 
-#libft
-LFT = ./libft/
-LFT_FILES = ft_isdigit ft_bzero ft_memset ft_isascii ft_strlen
-LFT_CFILES = $(patsubst %, $(LFT)%.c, $(LFT_FILES))
-LFT_OFILES = $(patsubst %, %.o, $(LFT_FILES))
-LFT_LIB = $(addprefix $(LFT), ft.a)
-LFT_INC = -I $(LFT)includes/
+FLAGS = -Wall -Werror -Wextra
 
+SRCS = ft_printf.c				\
+		parse_prespecifiers.c	\
+		parse_specifier.c 		\
+		int_conv.c				\
+		uint_handlers.c			\
+		str_char_handlers.c		\
+		special_handlers.c		\
+		printf_gen_utils.c		\
+		printf_utf_utils.c		\
+		printf_int_utils.c		\
+
+OBJ = $(SRCS:.c=.o)
+
+HEADERS = ft_printf.h libft/libft.h
+
+all: $(NAME)
+	@echo "\033[0;32mlibftprintf.a compiled successfully\033[0m"
+
+%.o: %.c
+	@gcc $(FLAGS) -o $@ -c $<
+
+libft/libft.a:
+	@make -C ./libft
+
+$(NAME): libft/libft.a $(OBJ)
+	@ar rcs $(NAME) $(OBJ) libft/*.o
+
+clean:
+	@/bin/rm -f $(OBJ)
+	@make -C ./libft clean
+
+fclean: clean
+	@/bin/rm -f $(NAME)
+	@make -C ./libft fclean
+
+re: fclean all
 
 .PHONY: all clean fclean re
-
-all : $(LFT_LIB) $(NAME)
-
-$(LFT_LIB):
-	make -C $(LFT)
-
-$(OFILES) :
-	gcc $(CFLAGS) -c -I $(INCLUDES) $(LFT_INC) $(LFT_CFILES) $(CFILES)
-
-$(NAME) : $(OFILES)
-	ar rc $(NAME) $(OFILES) $(LFT_OFILES)
-	ranlib $(NAME)
-
-clean :
-	make -C $(LFT) clean
-	$(RM) $(OFILES)
-	$(RM) $(LFT_OFILES)
-
-fclean : clean
-	make -C $(LFT) fclean
-	$(RM) $(NAME)
-
-re : fclean all
--- str_char_handlers.c
+++ str_char_handlers.c
@@ -6,7 +6,7 @@
 /*   By: lulebugl <lulebugl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 13:12:45 by lulebugl          #+#    #+#             */
-/*   Updated: 2019/11/04 13:27:08 by lulebugl         ###   ########.fr       */
+/*   Updated: 2019/11/04 13:33:54 by lulebugl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,49 +38,6 @@ void			handle_char(t_info *info)
 	buff(info, &c, 1);
 	if (fg & WIDTH_FLAG && (fg & MINUS_FLAG))
 		pad_width(info, 1);
-}
-
-/*
-**	calc_precision()
-**	Because utf-8 chars can be more than 1 'char' wide, we might have situations
-**	where the precision specified has more characters than number of printable
-**	utf-9 chars.
-**
-**	Example: ft_printf("|%15.4S|", L"我是一只猫。");
-**	The precision specified is 4 but the utf char '我'' is 3 'chars' wide
-**	and the '是' char cannot be printed with only 1 'char' of precision left.
-**
-**	Therefore use a recursive call to recalculate our precision so that the
-**	precision will be trimmed to how ever many utf chars can be
-**	successfully displayed.
-**
-**	1) If the wide str is at the end or we've exhausted our precision,
-**	   return our new_prec(ision)
-**	2) Otherwise if the current wide str char is 1 byte long subtract 1 from
-**	   our desired precision and add one to our new_prec(ision).
-**	3) Otherwise if the current wide str char is 2 bytes long and we have at
-**	   least 2 more counts in our desired precision then subtract 2 from our
-**	   desired precision and add 2 to our new_prec(ision).
-**	4) Some more steps...
-**	5) The process ends when we either have a wide char larger than 0x10FFFF
-**	   or when we no longer have enough of our desired precision to hold another
-**	   wide char.
-*/
-
-size_t			calc_precision(wchar_t *str, int precision, size_t new_prec)
-{
-	if (*str == '\0' || precision == 0)
-		return (new_prec);
-	else if (*str <= 0x007F)
-		return (calc_precision(str + 1, precision - 1, new_prec + 1));
-	else if (*str <= 0x07FF && precision >= 2)
-		return (calc_precision(str + 1, precision - 2, new_prec + 2));
-	else if (*str <= 0xFFFF && precision >= 3)
-		return (calc_precision(str + 1, precision - 3, new_prec + 3));
-	else if (*str <= 0x10FFFF && precision >= 4)
-		return (calc_precision(str + 1, precision - 4, new_prec + 4));
-	else
-		return (new_prec);
 }
 
 /*
